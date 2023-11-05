ffc8d968ef2371ab7aeb5e7c5d49f2b842d497a0
shevrel <alexhartl93@googlemail.com>
2023-11-05 16:42:28 +0100

    Add methods to gameSystem and createPlayer
    gameSystem should take care of the game flow, so the following
    methods were added:
    initGame, checkGameStatus and changeTurnToPlay.
    The player objects now have a field for the turn to play.

--- script.js
+++ script.js
@@ -2,22 +2,38 @@ const gameBoard = (function() {
     const gameBoard = [];
 
     const addMarker = (marker, position) => {
+        // add marker at specific position decided by mouseclick
         gameBoard[position] = marker;
     }
-
-
-    return {addMarker}
+    return {addMarker};
 })();
 
-const gameSystem = (function() {
-    const checkGameStatus = () => {
-
+const gameSystem = (function(player1, player2) {
+    const initGame = () => {
+        // greet players
+        // clear fields
+        // shows which turn to play
     }
+    const checkGameStatus = () => {
+        // check if someone has won
+        // else check if all fields are full
+    }
+    const changeTurnToPlay = () => {
+        // change players turn
+        player1.changeTurnToPlay
+        player2.changeTurnToPlay
+    }
+    return {initGame, checkGameStatus, changeTurnToPlay};
 })();
 
 function createPlayer (name, marker) {
-    return {name, marker};
+    let turnToPlay = false;
+    const getTurnToPlay = () => turnToPlay;
+    const changeTurnToPlay = () => {
+        turnToPlay = !turnToPlay;
+    }
+    return {name, marker, getTurnToPlay, changeTurnToPlay};
 }
 
-const player1 = createPlayer("Player 1", "x");
-const player2 = createPlayer("Player 1", "o");
+const pppplayer1 = createPlayer("Player 1", "x");
+const ppppplayer2 = createPlayer("Player 2", "o");
